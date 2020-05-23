6828ab9e374b0e6bbb364827a0e1b8373479b017
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-05-23 10:11:13 +0100

    Initial check-in, mailicious app.

--- mailicious/mail.py
+++ mailicious/mail.py
@@ -3,8 +3,8 @@
 import feedparser as f
 import markovify as m
 
-_T = [[e.title, e.summary]
-      for e in f.parse('feed://www.dailymail.co.uk/articles.rss').entries]
+_URL = 'feed://www.dailymail.co.uk/articles.rss'
+_ENTS = [[e.title, e.summary] for e in f.parse(_URL).entries]
 
 for _ in range(100):
-    print(m.Text([j for sub in _T for j in sub]).make_short_sentence(140))
+    print(m.Text([x for ent in _ENTS for x in ent]).make_short_sentence(140))
